85bc197cad758d153555178ecade0984b9d7e597
min8grace <56316476+min8grace@users.noreply.github.com>
2023-05-25 09:38:44 -0700

    css update

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,24 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
+
 function Movie({id, coverImg, title, year, summary, genres}){
     return (
-        <div> 
-            <img src={coverImg} alt={title} className={StyleSheet.movie__img}/>
-            <h2>
-                <Link to={`/movie/${id}`}>{title}</Link>
-            </h2>
-            <h3 className={StyleSheet.movie__year}>{year}</h3>
-            <p>{summary.length > 235 ? `${summary.slice(0,235)}... ` : summary}</p>
-            <ul className={StyleSheet.movie__genres}>
-            { genres.map( gen=> ( 
-                <li key={gen}>{gen}</li>
-            ))} 
-            </ul>
+        <div className={styles.movie}>
+        <img src={coverImg} alt={title} className={styles.movie__img} />
+        <div>
+          <h2 className={styles.movie__title}>
+            <Link to={`/movie/${id}`}>{title}</Link>
+          </h2>
+          <h3 className={styles.movie__year}>{year}</h3>
+          <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+          <ul className={styles.movie__genres}>
+            {genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
         </div>
+      </div>
     );
 };
 Movie.propTypes = {
